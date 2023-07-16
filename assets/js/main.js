2bfd87a28d44349482fed98f484714e32b4a7d28
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-07-17 04:40:35 +0600

    complete service section with JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,27 @@ tabs.forEach((tab) => {
   });
 });
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloses = document.querySelectorAll(".services__modal-close");
 
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
