d81190d1873e5134438ed232b0a77b10bf6f3c63
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-07-23 04:15:36 +0600

    added swiper js and complete testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ modalCloses.forEach((modalClose) => {
   });
 });
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop: true,
   navigation: {
@@ -102,7 +102,23 @@ let swiper = new Swiper(".portfolio__container", {
   keyboard: true,
 });
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
