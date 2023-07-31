499f65b244b6efbb586e6327554087272712b7af
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-08-01 03:05:34 +0600

    added scrool up icon in bottom

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,12 +143,13 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
-  const nav = document.getElementById('header')
+function scrollHeader() {
+  const nav = document.getElementById("header");
   // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-  if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
 
 /*==================== DARK LIGHT THEME ====================*/
